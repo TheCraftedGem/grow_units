8b5dfd0077398922e53062b1749a18148f6cf6e1
Melvin Cedeno <cedenomelvin@gmail.com>
2020-07-11 23:50:59 -0600

    removes extra endpoint

--- lib/grow_units/endpoint.ex
+++ lib/grow_units/endpoint.ex
@@ -37,7 +37,4 @@ defmodule GrowUnits.Endpoint do
 
   # A catchall route, 'match' will match no matter the request method,
   # so a response is always returned, even if there is no route to match.
-  match _ do
-    send_resp(conn, 404, "oops... Nothing here :(")
-  end
 end
